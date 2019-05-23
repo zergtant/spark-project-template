81fa302daa7d4f0eeaa8e5ee79e1b539be63b0bb
hengtao tantai <zergtant@qq.com>
2019-05-23 16:35:15 +0800

    自动设置sparkhome
    spark session，spark content 和hive支持

--- src/main/scala/tant/spark/template/App.scala
+++ src/main/scala/tant/spark/template/App.scala
@@ -12,7 +12,16 @@ object App  {
     println( "Hello World!" )
     val conf = new SparkConf().setMaster("local[2]")
       .setAppName("template-test")
-    val sc = new SparkContext(conf)
+    val builder = SparkSession.builder.config(conf)
+    if (System.getenv("SPARK_HOME") != null) {
+      conf.setSparkHome(System.getenv("SPARK_HOME"))
+    }
+    //创建spark session
+    val spark = builder
+      .enableHiveSupport()
+      .getOrCreate()
+    //使用spark session创建spark content
+    var sc=spark.sparkContext
     val x = List(1, 2, 3, 4)
     val sparkx=sc.parallelize(x)
     println(sparkx.count())
